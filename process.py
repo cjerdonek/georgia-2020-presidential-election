40860f843ba5c2bc8e80f03774dddd095de86801
Chris Jerdonek <chris.jerdonek@gmail.com>
2020-11-29 06:21:43 -0800

    Add undervotes and overvotes.

--- process.py
+++ process.py
@@ -16,9 +16,16 @@ import json
 import logging
 from pathlib import Path
 import time
+import xml.etree.ElementTree as ET
+from zipfile import ZipFile
 
 import requests
 
+
+CACHE_DIR = Path('cache')
+DOWNLOADS_DIR = Path('downloads')
+
+VOTE_TOTAL_KEYS = ['BC', 'Under', 'Over']
 
 _log = logging.getLogger()
 
@@ -27,17 +34,46 @@ def get_input_path(name):
     return Path('input-data') / name
 
 
+def read_json(path):
+    with open(path) as f:
+        data = json.load(f)
+
+    return data
+
+
+def write_json(path, data):
+    _log.info(f'writing json: {path}')
+    with open(path, mode='w') as f:
+        json.dump(data, f, sort_keys=True, indent='    ')
+
+
+def make_zero_vote_totals():
+    return {key: 0 for key in VOTE_TOTAL_KEYS}
+
+
+def unzip_file(zip_path, file_name):
+    suffix = Path(file_name).suffix
+
+    dir_path = zip_path.parent
+    new_path = zip_path.with_suffix(suffix)
+
+    with ZipFile(zip_path) as zipfile:
+        created_path = Path(zipfile.extract(file_name, path=dir_path))
+        _log.info(f'renaming to: {new_path}')
+        created_path.rename(new_path)
+
+    _log.info(f'removing: {zip_path}')
+    zip_path.unlink()
+
+
 def parse_county(line):
     # An example line:
     # Appling|105371|271560|11/16/2020 3:48:35 PM EST|16
     parts = line.split('|')
     name, id1, id2 = parts[:3]
+    name = name.replace('_', ' ')
 
-    # An example url:
-    # https://results.enr.clarityelections.com//GA/Appling/105371/271560/json/sum.json
-    url = f'https://results.enr.clarityelections.com//GA/{name}/{id1}/{id2}/json/sum.json'
-
-    return name, url
+    return name, id1, id2
 
 
 def iter_counties():
@@ -47,15 +83,14 @@ def iter_counties():
     # data['settings']['electiondetails']['participatingcounties']
     path = get_input_path('counties.json')
 
-    with open(path) as f:
-        data = json.load(f)
-        counties_data = data['participatingcounties']
+    data = read_json(path)
 
-        for line in counties_data:
-            name, url = parse_county(line)
-            name = name.replace('_', ' ')
+    counties_data = data['participatingcounties']
 
-            yield (name, url)
+    for i, line in enumerate(counties_data):
+        name, id1, id2 = parse_county(line)
+
+        yield (i, name, id1, id2)
 
 
 def get_county_names():
@@ -64,7 +99,7 @@ def get_county_names():
     """
     normalized_to_name = {}
 
-    for name, _ in iter_counties():
+    for i, name, *_ in iter_counties():
         normalized = name.upper()
         normalized_to_name[normalized] = name
 
@@ -77,35 +112,87 @@ def get_index_to_county_name():
     """
     index_to_name = {}
 
-    for i, (name, _) in enumerate(iter_counties()):
+    for i, name, *_ in iter_counties():
         index_to_name[i] = name
 
     return index_to_name
 
 
-def download(url, filename):
-    path = Path('downloads') / filename
+def download(url, path, target_path=None):
+    if target_path is None:
+        target_path = path
+
+    if target_path.exists():
+        _log.info(f'already downloaded: {target_path}')
+        return False
 
     _log.info(f'downloading: {url}')
     r = requests.get(url)
-    path.write_text(r.text)
+    if r.status_code != 200:
+        raise RuntimeError(f'got status code: {r.status_code}')
+
+    with open(path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=128):
+            f.write(chunk)
+
     _log.info(f'wrote to: {path}')
 
+    return True
 
-def download_county_results():
-    for i, (name, url) in enumerate(iter_counties()):
+
+def download_county_results(url_format, subdir, zip_name=None):
+    output_dir = DOWNLOADS_DIR / subdir
+
+    if not output_dir.exists():
+        output_dir.mkdir()
+
+    for i, name, id1, id2 in iter_counties():
         name = name.replace(' ', '_')
-        filename = f'{i:03}-{name}.json'
+        url = url_format.format(name=name, id1=id1, id2=id2)
+        ext = url.split('.')[-1]
 
-        download(url, filename)
-        _log.info('sleeping for 10 seconds before next download...')
-        time.sleep(10)
+        filename = f'{i:03}-{name}.{ext}'
+
+        path = output_dir / filename
+
+        if zip_name is not None:
+            suffix = Path(zip_name).suffix
+            target_path = path.with_suffix('.xml')
+
+        downloaded = download(url, path=path, target_path=target_path)
+
+        if not downloaded:
+            continue
+
+        if zip_name is not None:
+            unzip_file(path, file_name=zip_name)
+
+        _log.info('sleeping for 5 seconds before next download...')
+        time.sleep(5)
+
+
+def check_cache_path(base_name):
+    cache_path = CACHE_DIR / f'{base_name}.json'
+    if cache_path.exists():
+        _log.info(f'using cached data: {cache_path}')
+        data = read_json(cache_path)
+    else:
+        data = None
+
+    return (cache_path, data)
 
 
 def read_rla_totals():
+    base_name = 'rla-totals'
+    cache_path, totals = check_cache_path(base_name)
+    if totals is not None:
+        return totals
+
     normalized_to_name = get_county_names()
 
-    totals = {name: 0 for name in normalized_to_name.values()}
+    totals = {}
+    for name in normalized_to_name.values():
+        totals[name] = make_zero_vote_totals()
 
     # This CSV was downloaded from here:
     # https://sos.ga.gov/index.php/elections/historic_first_statewide_audit_of_paper_ballots_upholds_result_of_presidential_race
@@ -121,21 +208,48 @@ def read_rla_totals():
 
             name = row[0]
             name = normalized_to_name[name]
+            county_totals = totals[name]
 
-            numbers = [int(part) for part in row[3:]]
-            total = sum(numbers)
-            totals[name] += total
+            batch_type = row[2]
+
+            row_totals = [int(part) for part in row[3:]]
+
+            county_totals['BC'] += sum(row_totals)
+            county_totals['Under'] += row_totals[-2]
+            county_totals['Over'] += row_totals[-1]
+
+    write_json(cache_path, data=totals)
 
     return totals
 
 
-def read_official_totals():
+def iter_download_dir(subdir_name):
+    """
+    Yield pairs: (path, county_name).
+    """
+    index_to_name = get_index_to_county_name()
+
+    dir_path = DOWNLOADS_DIR / subdir_name
+
+    for path in sorted(dir_path.iterdir()):
+        # Skip files like ".DS_Store".
+        if not path.suffix:
+            continue
+
+        filename = path.name
+        index = int(filename.split('-')[0])
+        name = index_to_name[index]
+
+        yield path, name
+
+
+# This function is no longer needed.
+def read_official_json_totals():
     index_to_name = get_index_to_county_name()
 
     totals = {}
 
-    json_dir = Path('downloads')
-    for path in sorted(json_dir.iterdir()):
+    for path in sorted(DOWNLOADS_DIR.iterdir()):
         # Skip .gitkeep.
         if not path.suffix:
             continue
@@ -162,35 +276,138 @@ def read_official_totals():
     return totals
 
 
-def write_row(f, name, official, rla, last):
-    f.write(f'{name},{official},{rla},{last}\n')
+def get_votes_by_vote_type(element):
+    totals = {}
+
+    mapping = {
+        'Overvotes': 'Over',
+        'Undervotes': 'Under',
+    }
+    for vote_type in element.findall('VoteType'):
+        attrib = vote_type.attrib
+        name = attrib['name']
+        name = mapping[name]
+
+        total = attrib['votes']
+        totals[name] = int(total)
+
+    return totals
+
+
+def get_ballots_cast(root):
+    vt_element = root.find('VoterTurnout')
+    total = vt_element.attrib['ballotsCast']
+
+    return int(total)
+
+
+def read_detailxml_file(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    totals = {}
+    totals['BC'] = get_ballots_cast(root)
+
+    contest = root.find('Contest')
+    contest_name = contest.attrib['text']
+
+    # Make sure we have the right contest.
+    if not contest_name.startswith('President of the United States'):
+        raise RuntimeError(f'got contest name: {contest_name!r}')
+
+    new_totals = get_votes_by_vote_type(contest)
+    totals.update(new_totals)
+
+    return totals
+
+
+def read_official_totals():
+    base_name = 'detailxml'
+    cache_path, totals = check_cache_path(base_name)
+    if totals is not None:
+        return totals
+
+    totals = {}
+    for path, name in iter_download_dir('detailxml'):
+        try:
+            county_totals = read_detailxml_file(path)
+        except Exception:
+            raise RuntimeError(f'error reading: {path}')
+
+        totals[name] = county_totals
+
+    write_json(cache_path, data=totals)
+
+    return totals
+
+
+def compute_all_counties(totals):
+    all_totals = make_zero_vote_totals()
+
+    for county_totals in totals.values():
+        for key, total in county_totals.items():
+            all_totals[key] += total
+
+    return all_totals
+
+
+def add_all_totals(totals):
+    all_totals = compute_all_counties(totals)
+    totals['ALL'] = all_totals
+
+
+def write_row(f, values):
+    text = ','.join(str(value) for value in values)
+    f.write(f'{text}\n')
 
 
 def write_output(official_totals, rla_totals):
-    rla_total = 0
-    official_total = 0
+    add_all_totals(official_totals)
+    add_all_totals(rla_totals)
 
     rows = []
+    key_count = len(VOTE_TOTAL_KEYS)
     for name in sorted(official_totals):
-        rla = rla_totals[name]
-        official = official_totals[name]
-        row = (name, official, rla, rla - official)
-        rows.append(row)
+        rla_county_totals = rla_totals[name]
+        official_county_totals = official_totals[name]
 
-        rla_total += rla
-        official_total += official
+        pairs = [
+            ('Official', official_county_totals),
+            ('RLA', rla_county_totals),
+        ]
+
+        row = [name]
+        for prefix, totals in pairs:
+            for key in VOTE_TOTAL_KEYS:
+                row.append(totals[key])
+
+        for i in range(1, key_count + 1):
+            delta = row[i + key_count] - row[i]
+            row.append(delta)
+
+        rows.append(row)
 
     # Sort by the magnitude of the difference, in descending order,
     # then next alphabetically.
-    rows = sorted(rows, key=lambda row: (-1 * abs(row[-1]), name))
+    bc_delta_index = -1 * key_count
+
+    def sort_key(row):
+        # 1) Make the ALL row first.
+        return (row[0] != 'ALL', -1 * abs(row[bc_delta_index]), name)
+
+    rows = sorted(rows, key=sort_key)
+
+    headers = ['County']
+    for prefix in ('Official', 'RLA', 'Delta'):
+        for key in VOTE_TOTAL_KEYS:
+            header = f'{prefix}-{key}'
+            headers.append(header)
 
     path = Path('output.csv')
     with open(path, mode='w') as f:
-        write_row(f, 'County', 'Initial', 'RLA', 'Difference')
+        write_row(f, headers)
         for row in rows:
-            write_row(f, *row)
-
-        write_row(f, 'ALL', official_total, rla_total, rla_total - official_total)
+            write_row(f, row)
 
     _log.info(f'wrote: {path}')
 
@@ -198,8 +415,17 @@ def write_output(official_totals, rla_totals):
 def main():
     logging.basicConfig(level='INFO')
 
-    # Comment out this line after downloading the county results.
-    download_county_results()
+    # Some example urls:
+    # https://results.enr.clarityelections.com//GA/Appling/105371/271560/json/sum.json
+    # https://results.enr.clarityelections.com//GA/Fulton/105430/271723/reports/detailxml.zip
+    # url_format = 'https://results.enr.clarityelections.com//GA/{name}/{id1}/{id2}/json/sum.json'
+    # subdir = 'sumjson'
+    url_format = 'https://results.enr.clarityelections.com//GA/{name}/{id1}/{id2}/reports/detailxml.zip'
+    subdir = 'detailxml'
+
+    download_county_results(url_format, subdir=subdir, zip_name='detail.xml')
+
+    official_totals = read_official_totals()
 
     rla_totals = read_rla_totals()
 
@@ -218,9 +444,9 @@ def main():
     # correction though, as we don't know the true number of second sheet
     # ballot cards found during the RLA.  (We are using the number from
     # the official / pre-RLA totals as a proxy.)
-    rla_totals['Gwinnett'] -= 119461
-
-    official_totals = read_official_totals()
+    gwinnett_totals = rla_totals['Gwinnett']
+    for key in ('BC', 'Under'):
+        gwinnett_totals[key] -= 119461
 
     assert len(rla_totals) == 159
     assert set(rla_totals) == set(official_totals)
